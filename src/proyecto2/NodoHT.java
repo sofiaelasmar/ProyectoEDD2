c8ef49ebf1ed1440a9410476384ca15c28960d93
sofia <sofia@10.30.2.128>
2025-07-01 18:47:11 -0400

    NodoHT

---
+++ src/proyecto2/NodoHT.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author sofia
+ */
+public class NodoHT {
+    String secuencia;
+    
+    
+    public NodoHT(String sec){
+        this.secuencia = sec;
+    }
+}
