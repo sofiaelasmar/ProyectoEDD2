7f5f2ac3130981d4a069262e2b3968948eaa0d8a
Isolar03 <Isolar@correo.unimet.edu.ve>
2025-07-03 03:18:46 -0400

    NodoAminoacido

---
+++ src/proyecto2/NodoAminoacido.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author iker
+ */
+public class NodoAminoacido {
+    String[] cadenas;
+    String abreviacion;
+
+    public NodoAminoacido(String[] cadenas, String abreviacion) {
+        this.cadenas = cadenas;
+        this.abreviacion = abreviacion;
+    }
+    
+    
+}
