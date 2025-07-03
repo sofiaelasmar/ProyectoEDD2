12c70a057e8dac4eec1fc78da1666084ef071aae
Isolar03 <Isolar@correo.unimet.edu.ve>
2025-07-03 03:18:12 -0400

    Arreglo txt

--- src/proyecto2/LectorTXT.java
+++ src/proyecto2/LectorTXT.java
@@ -15,6 +15,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class LectorTXT {
+
     private HashTable hashTable; // Tabla hash para almacenar las secuencias
     private BST bstFrecuencias; // Árbol binario de búsqueda para frecuencias
 
@@ -45,24 +46,29 @@ public class LectorTXT {
     }
 
     /**
-     * Método para procesar la secuencia de ADN y añadir fragmentos a la tabla hash y al BST.
+     * Método para procesar la secuencia de ADN y añadir fragmentos a la tabla
+     * hash y al BST.
+     *
      * @param secuencia La secuencia de ADN a procesar.
      */
     private void procesarSecuencia(String secuencia) {
         for (int i = 0; i <= secuencia.length() - 3; i++) {
             String fragmento = secuencia.substring(i, i + 3);
             hashTable.añadir(fragmento); // Añadir a la tabla hash
-
-            // Obtener el nodo de la tabla hash para la frecuencia
-            NodoHT nodo = hashTable.buscar(fragmento);
-            if (nodo != null) {
-                bstFrecuencias.insert(nodo.frecuencia, fragmento); // Insertar en el BST
+        }
+        for (int i = 0; i < hashTable.size; i++) {
+            try {
+                this.bstFrecuencias.insert(hashTable.secuencias[i].frecuencia, hashTable.secuencias[i].secuencia);
+            } catch (Exception e) {
+                continue;
             }
         }
+
     }
 
     /**
      * Método para obtener la tabla hash.
+     *
      * @return La tabla hash de secuencias.
      */
     public HashTable getHashTable() {
@@ -71,10 +77,10 @@ public class LectorTXT {
 
     /**
      * Método para obtener el árbol de frecuencias.
+     *
      * @return El árbol binario de búsqueda de frecuencias.
      */
     public BST getBstFrecuencias() {
         return bstFrecuencias;
     }
 }
-
